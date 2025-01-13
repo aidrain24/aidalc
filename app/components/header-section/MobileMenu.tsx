11f7e83089ce709112b71225fe634645e06cfe30
Aidrain Alcantara <aidrainalcantara@gmail.com>
2025-01-13 23:08:06 +0800

    changes in messanger links

--- app/components/header-section/MobileMenu.tsx
+++ app/components/header-section/MobileMenu.tsx
@@ -64,7 +64,7 @@ export default function MobileMenu({
         <Link
           className="p-4 flex-1 flex justify-center w-full rounded-xl h-fit text-4xl visited:bg-[#E3D3BE] bg-gradient-to-r from-[#d9d9d915] to-[#7373731f] std-backdrop-blur"
           target="_blank"
-          href="https://www.facebook.com/messages/t/100000525454146"
+          href="http://m.me/aidrain24"
           data-blobity-radius="10"
         >
         <Icon icon="hugeicons:messenger" />
